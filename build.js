53ca0f3a67894dc86a71d179d1a9a602c9f6c832
clux <analsandblaster@gmail.com>
2011-12-21 10:36:49 +0000

    simple new build system

---
+++ build.js
@@ -0,0 +1,16 @@
+var fs = require('fs')
+  , md = require("node-markdown").Markdown;
+
+function generateDocs() {
+  var head = fs.readFileSync('head.html', 'utf8')
+    , tail = fs.readFileSync('tail.html', 'utf8')
+    , files = ['api', 'cli', 'xcjs', 'plugins', 'modularity', 'require'];
+
+  files.forEach(function (file) {
+    var out = fs.readFileSync('../docs/' + file + '.md', 'utf8');
+    fs.writeFileSync('./docs/' + file + '.html', head + md(out) + tail);
+  });
+}
+
+generateDocs();
+
