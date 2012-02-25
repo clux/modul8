b47a4f2dc91e5b556d229e17a3863943bcfaca7a
clux <analsandblaster@gmail.com>
2012-02-25 13:29:00 +0000

    0.16.1 docs

--- build.js
+++ build.js
@@ -4,8 +4,16 @@ var fs = require('fs')
 function generateDocs() {
   var head = fs.readFileSync('head.html', 'utf8')
     , tail = fs.readFileSync('tail.html', 'utf8')
-    , files = ['api', 'cli', 'xcjs', 'plugins', 'modularity', 'npm', 'require'];
-
+    , files = [
+        'api'
+      , 'tinyapi'
+      , 'cli'
+      , 'xcjs'
+      , 'plugins'
+      , 'modularity'
+      , 'npm'
+      , 'require'
+    ];
   files.forEach(function (file) {
     var out = fs.readFileSync('../docs/' + file + '.md', 'utf8');
     fs.writeFileSync('./docs/' + file + '.html', head + md(out) + tail);
