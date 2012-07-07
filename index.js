0a6af7096d3791b7b804d01fc9874a09a8468adf
clux <analsandblaster@gmail.com>
2012-07-07 18:46:35 +0100

    backwards compat with 0.6

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var fs = require('fs')
-  , join = require('path').join
-  , modul8 = require('./lib/api');
+  , path = require('path')
+  , join = require('path').join;
 
+fs.existsSync || (fs.existsSync = path.existsSync);
+
+var modul8 = require('./lib/api');
 modul8.minifier   = require('./lib/plugins/minifier');
 modul8.testcutter = require('./lib/plugins/testcutter');
 modul8.version    = require('./package').version;
